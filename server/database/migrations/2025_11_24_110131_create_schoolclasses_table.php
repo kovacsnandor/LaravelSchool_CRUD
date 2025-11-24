794464fef8201bda0cdcb916ecdb93534a510495
ngy.ptrk._ <nagydpatrik@gmail.com>
2025-11-24 12:44:03 +0100

    schoolclass modositas

--- server/database/migrations/2025_11_24_110131_create_schoolclasses_table.php
+++ server/database/migrations/2025_11_24_110131_create_schoolclasses_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('schoolclasses', function (Blueprint $table) {
             $table->id();
-            $table->string('className', 10)->notNull();
+            $table->string('osztalyNev', 10)->notNull();
             $table->timestamps();
         });
     }
