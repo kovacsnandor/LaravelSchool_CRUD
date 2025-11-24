175e7929e6d1ebea0c2fe9c61efaefa83563b53d
ngy.ptrk._ <nagydpatrik@gmail.com>
2025-11-24 12:43:38 +0100

    students tabla es migracio kesz

---
+++ server/app/Http/Controllers/StudentsController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\students;
+use App\Http\Requests\StorestudentsRequest;
+use App\Http\Requests\UpdatestudentsRequest;
+
+class StudentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorestudentsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(students $students)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatestudentsRequest $request, students $students)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(students $students)
+    {
+        //
+    }
+}
