c616eee504684ad5a571b15e3027536f3623d73f
dsf <ngf@gmail.com>
2025-11-25 08:20:18 +0100

    playingsports tabla es migracio

---
+++ server/app/Http/Controllers/PlayingsportsController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\playingsports;
+use App\Http\Requests\StoreplayingsportsRequest;
+use App\Http\Requests\UpdateplayingsportsRequest;
+
+class PlayingsportsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreplayingsportsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(playingsports $playingsports)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateplayingsportsRequest $request, playingsports $playingsports)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(playingsports $playingsports)
+    {
+        //
+    }
+}
