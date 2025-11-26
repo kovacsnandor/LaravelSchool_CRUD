55b76d4f10b0c462ffa674280aefc0c291a150e1
Bmarc015 <bartamarcell0@gmail.com>
2025-11-26 11:30:00 +0100

    PlayingsportController/show fuggveny kesz

--- server/app/Http/Controllers/PlayingsportController.php
+++ server/app/Http/Controllers/PlayingsportController.php
@@ -40,17 +40,36 @@ class PlayingsportController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StorePlayingsportRequest $request)
+    public function store(int $id)
     {
-        //
+       
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Playingsport $playingsports)
+    public function show(int $id)
     {
         //
+         //
+          $row = Playingsport::find($id);
+        if ($row) {
+            # code...
+            $status = 200;
+            $data = [
+                'message' => 'OK',
+                'data' => $row
+            ];
+        } else {
+            $status = 404;
+            $data = [
+                'message' => "Not_Found id: $id ",
+                'data' => null
+            ];
+        }
+
+        return response()->json($data, $status, options: JSON_UNESCAPED_UNICODE);
+        
     }
 
     /**
