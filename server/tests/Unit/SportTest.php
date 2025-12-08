54df18bae7d810307d51e6ecf6b895220e79725f
bmate07 <b.mate.j07@gmail.com>
2025-12-08 12:24:24 +0100

    SportTest kesz

--- server/tests/Unit/SportTest.php
+++ server/tests/Unit/SportTest.php
@@ -26,7 +26,6 @@ class SportTest extends TestCase
     public function test_exists_sports_table(): void
     {
         //Ellenőrizze, hogy megvan-e a tábla
-
         $this->assertTrue(Schema::hasTable($this->table), "A sports tábla nem létezik");
     }
 
@@ -35,34 +34,28 @@ class SportTest extends TestCase
     {
         //Ellenőrizze, hogy megvannak-e a tábla mezői
         $this->assertTrue(Schema::hasColumn($this->table, $expectedColumn), "A '$expectedColumn' oszlop nem letezik");
-
     }
 
     #[DataProvider('expectedSchemaDataProvider')]
     public function test_the_sports_table_columns_have_the_expected_types(string $expectedColumn, string $expectedType)
     {
         //Ellenőrizze, hogy jók-e a típusai
-
-        $columns = Schema::getColumnListing('sports');
-
+        $columns = Schema::getColumnListing($this->table);
 
         $this->assertEmpty(
             array_diff(array_keys($this->expectedSchema), $columns),
             'Hiányzó oszlopok a sports táblában.'
         );
 
+        foreach ($this->expectedSchema as $expectedColumn => $expectedType) {
 
-        foreach ($this->expectedSchema as $columnName => $expectedLaravelType) {
+            $actualDbSqlType = Schema::getColumnType($this->table, $expectedColumn);
 
-            $actualDbSqlType = Schema::getColumnType('sports', $columnName);
-
-            $isTypeMatch = $actualDbSqlType == $expectedLaravelType;
+            $isTypeMatch = $actualDbSqlType == $expectedType;
             $this->assertTrue(
                 $isTypeMatch,
-                "A '{$columnName}' oszlop típusa nem egyezik. Várt: '{$expectedLaravelType}', Kapott DB-típus: '{$actualDbSqlType}'."
+                "A '{$expectedColumn}' oszlop típusa nem egyezik. Várt: '{$expectedType}', Kapott DB-típus: '{$actualDbSqlType}'."
             );
         }
     }
-
-
 }
