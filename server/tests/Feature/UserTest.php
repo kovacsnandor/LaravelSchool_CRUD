2848166260c16bb700b846760b111a653f80fe60
kovacsnandor <kovacsnandor@gmail.com>
2025-12-08 13:57:48 +0100

    login, logout, test_login

--- server/tests/Feature/UserTest.php
+++ server/tests/Feature/UserTest.php
@@ -11,8 +11,43 @@ use Tests\TestCase;
 class UserTest extends TestCase
 {
     use DatabaseTransactions;
+
+    private function login(string $email = 'admin@example.com', string $password = '123')
+    {
+        $uri = '/api/users/login';
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+        ];
+        $data = [
+            'email' => $email,
+            'password' => $password,
+        ];
+        $response = $this
+            ->withHeaders($headers)
+            ->postJson($uri, $data);
+
+        return $response;
+    }
+
+    private function logout($token)
+    {
+        $uri = '/api/users/logout';
+        $headers = [
+            'Accept' => 'application/json',
+            'Authorization' => "Bearer $token"
+        ];
+        $response = $this
+            ->withHeaders($headers)
+            ->postJson($uri);
+
+        return $response;            
+    }
+
+
+
     //Létrehozunk és törlünk usert
-        public function test_create_delete_user(): void
+    public function test_create_delete_user(): void
     {
 
         $data = [
@@ -21,12 +56,13 @@ class UserTest extends TestCase
             'password' => '123',
         ];
         $uri = '/api/users';
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+        ];
 
         $response = $this
-            ->withHeaders([
-                'Content-Type' => 'application/json',
-                'Accept' => 'application/json',
-            ])
+            ->withHeaders($headers)
             ->postJson($uri, $data);
         // dd($response);
 
@@ -36,18 +72,39 @@ class UserTest extends TestCase
         $user = User::where('email', $data['email'])->first();
         $this->assertNotNull($user);
 
-        //user törlés, ez így tiltott
+        //user törlés, ez így tiltott, nem tudja letörölni.
         $id = $response->json('data')['id'];
         $uri = "/api/users/$id";
 
         $response = $this
-            ->withHeaders([
-                'Content-Type' => 'application/json',
-                'Accept' => 'application/json',
-            ])
+            ->withHeaders($headers)
             ->deleteJson($uri);
-            
-        $response->assertStatus(401);                
+
+        $response->assertStatus(401);
     }
 
+    public function test_login()
+    {
+        //Csinálok egy user-t
+        $data = [
+            'name' => 'Tanuló 3',
+            'email' => 'tanulo3@example.com',
+            'password' => '123',
+        ];
+        $uri = '/api/users';
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+        ];
+
+        $response = $this
+            ->withHeaders($headers)
+            ->postJson($uri, $data);
+
+        $response->assertStatus(201);
+
+        $token = $response->json('data')['token'];
+        dd($token);
+
+    }
 }
