0270abe0239fa15ae88679c072a291536b5bfa08
kovacsnandor <kovacsnandor@gmail.com>
2025-12-09 13:59:57 +0100

    test_table_user_login_post_delete_logout

--- server/tests/Feature/PingTest.php
+++ server/tests/Feature/PingTest.php
@@ -2,6 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\Playingsport;
+use App\Models\Schoolclass;
+use App\Models\Sport;
+use App\Models\Student;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -12,8 +17,39 @@ use PHPUnit\Framework\Attributes\DataProvider;
 class PingTest extends TestBase
 {
     use DatabaseTransactions;
+    private static $dataUser = [
+        "name" =>  "Tanuló3",
+        "email" => "tanulo3@example.com",
+        "password" => "123"
+    ];
 
-        public static function tablesGetDataProvider(): array
+    private static $dataSchoolclass = [
+        "osztalyNev" =>  "8d",
+    ];
+    private static $dataSport = [
+        "sportNev" =>  "Táj csí1",
+    ];
+
+    private static $dataStudent = [
+        "diakNev" => "Tóth Márton",
+        "schoolclassId" => 3,
+        "neme" => true,
+        "iranyitoszam" => "4024",
+        "lakHelyseg" => "Debrecen",
+        "lakCim" => "Kossuth utca 18.",
+        "szulHelyseg" => "Nyíregyháza",
+        "szulDatum" => "2007-11-23",
+        "igazolvanyszam" => "AB123456",
+        "atlag" => 3.7,
+        "osztondij" => 25000
+    ];
+
+    private static $dataPlayngSport = [
+        "studentId" => 1,
+        "sportId" => 8
+    ];
+
+    public static function tablesGetDataProvider(): array
     {
         return [
             'get users admin: 200' => ['users', 'admin@example.com', '123', 200],
@@ -39,6 +75,29 @@ class PingTest extends TestBase
         ];
     }
 
+    public static function tablesPostDeleteDataProvider(): array
+    {
+        return [
+            'post-delete users admin' => ['users', 'admin@example.com', '123', true, true, self::$dataUser],
+            'post-delete sports admin' => ['sports', 'admin@example.com', '123', true, true, self::$dataSport],
+            'post-delete schoolclasses admin' => ['schoolclasses', 'admin@example.com', '123', true, true, self::$dataSchoolclass],
+            'post-delete students admin' => ['students', 'admin@example.com', '123', true, true, self::$dataStudent],
+            'post-delete playingsports admin' => ['playingsports', 'admin@example.com', '123', true, true, self::$dataPlayngSport],
+
+            'post-delete users tanar' => ['users', 'tanar@example.com', '123', true, false, self::$dataUser],
+            'post-delete sports tanar' => ['sports', 'tanar@example.com', '123', false, false, self::$dataSport],
+            'post-delete schoolclasses tanar' => ['schoolclasses', 'tanar@example.com', '123', false, false, self::$dataSchoolclass],
+            'post-delete students tanar' => ['students', 'tanar@example.com', '123', true, true, self::$dataStudent],
+            'post-delete playingsports tanar' => ['playingsports', 'tanar@example.com', '123', false, false, self::$dataPlayngSport],
+
+            'post-delete users diak1' => ['users', 'diak1@example.com', '123', true, false, self::$dataUser],
+            'post-delete sports diak1' => ['sports', 'diak1@example.com', '123', false, false, self::$dataSport],
+            'post-delete schoolclasses diak1' => ['schoolclasses', 'diak1@example.com', '123', false, false, self::$dataSchoolclass],
+            'post-delete students diak1' => ['students', 'diak1@example.com', '123', false, false, self::$dataStudent],
+            'post-delete playingsports diak1' => ['playingsports', 'diak1@example.com', '123', false, false, self::$dataPlayngSport],
+        ];
+    }
+
 
     //Attribútum: Megmonjuk, hogy mi a dataPrivider-e a függvénynek
     #[DataProvider('tablesGetDataProvider')]
@@ -55,6 +114,70 @@ class PingTest extends TestBase
         $uri = "/api/$route";
         $response = $this->myGet($uri, $token);
         $response->assertStatus($expectedStatus);
+
+        //logout
+        $response = $this->logout($token);
+        $response->assertStatus(200);
+    }
+
+    #[DataProvider('tablesPostDeleteDataProvider')]
+    public function test_table_user_login_post_delete_logout($route, $email, $password, $expectedPostAccess,  $expectedDeletetAccess, $data)
+    {
+        //login
+        $response = $this->login($email, $password);
+        $response->assertStatus(200);
+
+        //token
+        $token = $this->myGetToken($response);
+
+        //post tábla
+        $uri = "/api/$route";
+        $response = $this->myPost($uri, $data, $token);
+        if ($expectedPostAccess) {
+            //siker
+            $response->assertSuccessful();
+        } else {
+            //hiba
+            $response->assertClientError();
+        }
+
+
+        //delete tábla
+        $table = $route;
+        $uri = "/api/$table";
+        $response = $this->myPost($uri, $data, $token);
+        if ($expectedPostAccess) {
+            # code...
+            $response->assertSuccessful();
+        } else {
+            $response->assertClientError();
+        }
+
+        if ($table == "sports") {
+            $response = Sport::factory()->create([
+                "sportNev" => "Táj csí 3"
+            ]);
+        } else if ($table == 'users') {
+            $response = User::factory()->create();
+        } else if ($table == 'schoolclasses') {
+            $response = Schoolclass::factory()->create([
+                "osztalyNev" => "8x"
+            ]);
+        } else if ($table == 'students') {
+            $response = Student::factory()->create();
+        } else if ($table == 'playingsports') {
+            $response = Playingsport::factory()->create();
+        }
+
+        $id = $response->id;
+        $uri = $uri . "/$id";
+        $response = $this->myDelete($uri, $token);
+        if ($expectedDeletetAccess) {
+            # code...
+            $response->assertSuccessful();
+        } else {
+            $response->assertClientError();
+        }
 
         //logout
         $response = $this->logout($token);
