c5ef0aa7e8dee17c634ea54b81dbe2c997205c54
kovacsnandor <kovacsnandor@gmail.com>
2025-12-09 11:04:16 +0100

    request rest frissítés

--- server/tests/TestBase.php
+++ server/tests/TestBase.php
@@ -36,5 +36,70 @@ abstract class TestBase extends TestCase
         return $response;            
     }
 
+    protected function myGetToken($response)
+    {
+        $token = $response->json('data')['token'];
+        return $token;
+    }
+
+    protected function myGet(string $uri, string $token)
+    {
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $token
+        ];
+        $response = $this
+            ->withHeaders($headers)
+            ->get($uri);
+        return $response;    
+    }
+
+
+    protected function myPost(string $uri, array $data, string $token)
+    {
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $token
+        ];
+        
+        $response = $this
+            ->withHeaders($headers)
+            ->postJson($uri, $data); // postJson() az adatok JSON-ként való küldéséhez
+            
+        return $response;
+    }
+
+    protected function myPatch(string $uri, array $data, string $token)
+    {
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $token
+        ];
+        
+        $response = $this
+            ->withHeaders($headers)
+            ->patchJson($uri, $data); // patchJson() az adatok JSON-ként való küldéséhez
+            
+        return $response;
+    }
+
+        protected function myDelete(string $uri, string $token)
+    {
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $token
+        ];
+        
+        $response = $this
+            ->withHeaders($headers)
+            ->delete($uri); // delete() metódus használata
+            
+        return $response;
+    }
+
 
 }
